fa3127262f1394c43bdc21af22f7a2f75a92c1b9
Віталій <vitaliy.shandar@gmail.com>
2024-10-14 18:17:58 +0300

    Adding a database, creating objects

---
+++ src/main/java/com/example/airlineops/AirlineOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.airlineops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineOpsApplication.class, args);
+	}
+
+}
